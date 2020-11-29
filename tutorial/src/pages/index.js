df01b99e925015efc7049807a0f179a569cb4b48
Iyad <iyadhb@gmail.com>
2020-11-29 20:10:18 +0000

    links and navbar

--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import NavBar from "../components/NavBar"
+import Navbar from '../components/NavBar'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+  <NavBar />
+  
+  Hello world!</div>
 }
