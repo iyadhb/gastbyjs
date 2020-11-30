7f6b4033d11849dbc765074a1ce0516ad28f1347
Iyad <iyadhb@gmail.com>
2020-11-30 08:58:58 +0000

    PageQuery

--- tutorial/src/pages/examples.js
+++ tutorial/src/pages/examples.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Header from '../examples/Header'
+import { compose, graphql } from 'gatsby'
 
-const examples = () => {
+
+const examples = ({data}) => {
+    const {site:{info:{author}}} = data
  return (
   <Layout>
    <h1>Hello from exaples page</h1>
    <Header />
+   <h5>author: {author}</h5>
   </Layout>
  )
 }
+
+export const data = graphql`
+query MyQuery {
+site {
+  info:siteMetadata {
+    person {
+      age
+      name
+    }
+    author
+    data
+    description
+    title
+  }
+}
+}
+`
 
 export default examples
