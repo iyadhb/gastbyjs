5e5d92cee1448da83ec79fdf8a9d6128cfe460ba
Iyad <iyadhb@gmail.com>
2020-11-30 10:23:45 +0000

    installed gatsby-image plugin

--- tutorial/gatsby-config.js
+++ tutorial/gatsby-config.js
@@ -15,6 +15,8 @@ module.exports = {
   }
   ,
   plugins: [
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
