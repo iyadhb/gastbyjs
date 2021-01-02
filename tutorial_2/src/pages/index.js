d7989850bc052e4005914d811df9339c8db7cb6f
Iyad <iyadhb@gmail.com>
2021-01-02 20:46:18 +0000

    Gatsby Styled Components

--- tutorial_2/src/pages/index.js
+++ tutorial_2/src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Layout from '../components/Layout'
+import {ExampleButton} from '../components/button'
+
 
 export default function Home() {
   return (
       <Layout>
           <h1>Hello People!</h1>
+          <ExampleButton>Click Me</ExampleButton>
       </Layout>
     
     )
