91f36229ef14c4cfd7ffa2d1bf4c242af6983e94
Iyad <iyadhb@gmail.com>
2021-01-02 22:01:26 +0000

    GraphQL - Field Alias

--- tutorial_2/src/examples/Header.js
+++ tutorial_2/src/examples/Header.js
@@ -1,29 +1,35 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import{ useStaticQuery, graphql} from 'gatsby'
 
-const ComponentName = () => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          author
-          data
-          description
-          title
-          person {
-            age
-            name
-          }
+const getData = graphql`
+        {
+        site {
+            info : siteMetadata {
+            author
+            data
+            description
+            title
+            person {
+                age
+                name
+            }
+            }
         }
-      }
-    }
-  `)
-  return (
-      <div>
-        <h2>{data.site.siteMetadata.person.name}</h2>
-        <h2>{data.site.siteMetadata.person.age}</h2>
-    </div>
-  )
+        }
+`
+
+const Header = () => {
+    const {site:{info:{title, person:{name}}}} = useStaticQuery(getData);
+   
+    return (
+        <div>
+          { /* <h1>title: {data.site.siteMetadata.title}</h1>
+            <h1>name: {data.site.siteMetadata.person.name}</h1>
+    <h1>name: {data.site.siteMetadata.person.age}</h1> */}
+        <h1>title: {title}</h1>
+        <h1>name: {name}</h1>
+        </div>
+    )
 }
 
-export default ComponentName
+export default Header
