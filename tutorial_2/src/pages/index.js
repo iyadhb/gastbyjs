6e8c91e43e24c6e2c5e455caa62a3cc6fbc3ee7e
Iyad <iyadhb@gmail.com>
2021-01-02 19:31:36 +0000

    creating NavBar

--- tutorial_2/src/pages/index.js
+++ tutorial_2/src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import {Link} from 'gatsby'
+import NavBar from '../components/Navbar'
 
 export default function Home() {
   return (
       <div>
+      <NavBar />
+      <h1>Hello People!</h1>
         <Link to="/blog/">Blog Page</Link>
       </div>
     
