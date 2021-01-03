3eb737382afed2e3e14fe2686bfaffe73bf7f118
Iyad <iyadhb@gmail.com>
2021-01-03 21:31:09 +0000

    allFile field & Query arguments

--- tutorial_2/gatsby-config.js
+++ tutorial_2/gatsby-config.js
@@ -14,5 +14,13 @@ module.exports = {
   }
   ,
   /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [`gatsby-plugin-styled-components`,
+    {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `${__dirname}/src/images/`,
+        },
+      },
+],
 }
