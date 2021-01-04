09aa27f1e499f47734d3615f072e4908af0dc1a8
Iyad <iyadhb@gmail.com>
2021-01-04 00:21:18 +0000

    Render Gatsby-Image

--- tutorial_2/src/examples/Images.js
+++ tutorial_2/src/examples/Images.js
@@ -1,22 +1,51 @@
 import React from 'react'
 import {graphql, useStaticQuery} from 'gatsby'
 import img from '../images/image-4.jpeg'
-import Images from 'gatsby-image'
+import Image from 'gatsby-image'
 
-const Image = () => {
+const getImages = graphql`
+{
+  fixed: file(relativePath: {eq: "image-3.jpg"}) {
+    childImageSharp {
+      fixed(width: 420, grayscale: true) {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
+  fluid: file(relativePath: {eq: "image-4.jpeg"}) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid_withWebp_tracedSVG
+
+      }
+    }
+  }
+}
+`
+
+const Images = () => {
+    const data = useStaticQuery(getImages)
+    console.log(data);
     return (
         <section className="images">
             <article className="single-image">
                 <h3>Basic Image</h3>
+                <img src={img} width="100%" />
             </article>
             <article className="single-image">
                 <h3>Fixed Image/blur</h3>
+                <Image fixed={data.fixed.childImageSharp.fixed} />
             </article>
             <article className="single-image">
                 <h3>fluid Image/svg</h3>
+                <Image fluid={data.fluid.childImageSharp.fluid} />
+                <div className="small">
+                    <Image fluid={data.fluid.childImageSharp.fluid} />
+                </div>
+
             </article>
         </section>
     )
 }
 
-export default Image
+export default Images
